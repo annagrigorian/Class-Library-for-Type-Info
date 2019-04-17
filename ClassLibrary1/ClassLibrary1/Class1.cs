44e12be86411d5d713ebed1ed7baef232cf4d479
Anna Grigorian <anna.grigorian11@gmail.com>
2019-04-17 23:53:50 +0400

    Class Library for getting Type Info

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace ClassLibrary1
+{
+    public class TypeInfo
+    {
+        private Type type;
+
+        public TypeInfo(object type)
+        {
+            try
+            {
+                this.type = type.GetType();
+            }
+            catch (ArgumentException exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+      
+        #region Info Methods
+
+        public void GetMethods()
+        {
+            Console.WriteLine();       
+            MethodInfo[] methods = type.GetMethods();
+            Console.WriteLine("*************Methods****************");
+            Console.WriteLine();
+            foreach (var method in methods)
+            {
+                Console.WriteLine("\t" + method.ToString());
+            }
+        }
+
+        public void GetProperties()
+        {
+            Console.WriteLine();
+            Console.WriteLine("*************Properties****************");
+            Console.WriteLine();
+            PropertyInfo[] properties = type.GetProperties();
+            foreach (var prop in properties)
+            {
+                Console.WriteLine("\t" + prop.ToString());
+            }
+        }
+
+        public void GetNestedTypes()
+        {
+            Console.WriteLine();
+            Console.WriteLine("*************Nested Types****************");
+            Console.WriteLine();
+            Type[] nestedTypes = type.GetNestedTypes();
+            foreach (var nestedType in nestedTypes)
+            {
+                Console.WriteLine("\t" + nestedType.ToString());
+            }
+        }
+
+        public void GetMembers()
+        {
+            Console.WriteLine();
+            Console.WriteLine("*************Members****************");
+            Console.WriteLine();
+            MemberInfo[] members = type.GetMembers();
+            foreach (var member in members)
+            {
+                Console.WriteLine("\t" + member.ToString());
+            }
+        }
+
+        public void GetConstructors()
+        {
+            Console.WriteLine();
+            Console.WriteLine("*************Constructors****************");
+            Console.WriteLine();
+            ConstructorInfo[] constructors = type.GetConstructors();
+            foreach (var constructor in constructors)
+            {
+                Console.WriteLine("\t" + constructor.ToString());
+            }
+        }
+
+        public void GetInterfaces()
+        {
+            Console.WriteLine();
+            Console.WriteLine("*************Interfaces****************");
+            Console.WriteLine();
+            Type[] interfaces = type.GetInterfaces();
+            foreach (var interFace in interfaces)
+            {
+                Console.WriteLine("\t" + interFace.ToString());
+            }
+        }
+
+        public void GetCustomAttributes()
+        {
+            Console.WriteLine();
+            Console.WriteLine("*************Custom Attributes****************");
+            Console.WriteLine();
+            IEnumerable<Attribute> attributes = type.GetCustomAttributes();
+            foreach (var attribute in attributes)
+            {
+                Console.WriteLine("\t" + attribute.ToString());
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TypeInfo t = new TypeInfo("i");
+
+            t.GetMethods();
+
+            t.GetInterfaces();
+
+            t.GetCustomAttributes();
+        }
+    }
+}
